e0907825a37369122c71b40d9c5b4110a3e29a28
Shahriyor <shahriyoradhamov@icloud.com>
2024-05-06 23:48:35 +0500

    hompage ui start

--- src/App.jsx
+++ src/App.jsx
@@ -24,26 +24,35 @@ import {
 } from './pages/index.jsx';
 
 function App() {
-	const {activeMenu} = useStateContext();
+	const {activeMenu, currentColor} = useStateContext();
 
 
 	return (
-		<div className=''>
-			<TooltipComponent content='Settings' position='Top'>
-				<button
-					type='button'
-					style={{ background: 'blue', borderRadius: '50%' }}
-					className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'
-				>
-					<FiSettings />
-				</button>
-			</TooltipComponent>
+		<div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content="Settings"
+              position="Top"
+            >
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: `${currentColor}`, borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+
+            </TooltipComponent>
+          </div>
 			{activeMenu ? (
 				<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white '>
 					<Sidebar />
 				</div>
 			) : (
-				<div className='w-0 dark:bg-secondary-dark-bg'>sidebar w-0</div>
+				<div className="w-0 dark:bg-secondary-dark-bg">
+					<Sidebar />
+				</div>
 			)}
 			<div
 				className={
