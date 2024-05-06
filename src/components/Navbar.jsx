cac73b0856f6ef1ad9eda1e1652cee33dc84f6fd
Shahriyor <shahriyoradhamov@icloud.com>
2024-05-06 22:32:59 +0500

    add context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsChatLeft } from "react-icons/bs";
+import { FiShoppingCart } from "react-icons/fi";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { RiNotification3Line } from "react-icons/ri";
+
 function Navbar() {
   return (
     <div>Navbar</div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
+import { ContextProvider } from './contexts/ContextProvider.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
